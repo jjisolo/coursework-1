0c477e9f5ec3d8b065cd6470c780b4f92a1bbdf9
jjisolo <jjisolo24@gmail.com>
2023-03-24 14:22:32 +0300

    Add more functions

--- source/rendering/RenderScene.cxx
+++ source/rendering/RenderScene.cxx
@@ -1,18 +1,51 @@
 #include "RenderScene.hxx"
 
-#define VectorFind(vector, item)         (std::find((vector).begin(), (vector).end(), (item)))
-#define VectorFindBinary(vector, item)   ((VectorFind((vector), (item)) != (vector).end()))
+#define VectorFind(vector, item) (std::find((vector).begin(), (vector).end(), (item)))
+#define VectorFindBinary(vector, item) ((VectorFind((vector), (item)) != (vector).end()))
 
 namespace GL {
 
-    RenderGroupID RenderScene::getDistinctRenderGroupID(void) const noexcept {
-        RenderGroupID renderGroupIdIterator{0};
+RenderGroupID RenderScene::getDistinctRenderGroupID(void) const noexcept
+{
+  RenderGroupID renderGroupIdIterator{ 0 };
 
-        // Iterate the indexes until we hit non-used number
-        while(!VectorFindBinary(m_vRenderGroupIDtoInternalID, renderGroupIdIterator))
-            ++renderGroupIdIterator;
+  // Iterate the indexes until we hit non-used number
+  while (!VectorFindBinary(m_vRenderGroupIDtoInternalID, renderGroupIdIterator))
+    ++renderGroupIdIterator;
 
-        return renderGroupIdIterator;
-    }
-
+  return renderGroupIdIterator;
 }
+
+void aliasRenderGroup(const RenderGroupID &renderGroupID, const std::string &renderGroupName) const noexcept
+{
+  // If the render group with the same name is already exists
+  if (m_mRenderGroupNames.contains(renderGroupName)) {
+    throw std::runtime_error("The name " + renderGroupName + " is already aliased to another render group");
+  }
+
+  // Otherwise perform the aliasing
+  m_mRenderGroupNames[renderGroupName] = id;
+}
+
+void pushToRenderGroup(const RenderGroupID &renderGroupID, const TextureDescriptor &textureDescriptor) const noexcept
+{
+  // The index of the render group associated with the renderGroupID
+  std::ptrdiff_t renderGroupIndex{ 0 };
+
+  try {
+    // Find out the index of the render group by its ID
+    renderGroupIndex = getRenderGroupByID(renderGroupID);
+  } catch (const std::invalid_argument &exception) {
+    // As soon as the specified ID is not in the table, the new
+    // render group should be created
+    m_vRenderGroupIDtoInternalID.push_back(renderGroupID);
+    m_vRenderGroups.emplace_back();
+
+    render_group_index = static_cast<std::ptrdiff_t>(m_vRenderGroupIDtoInternalID.size() - 1);
+  }
+
+  // Move the element to the render group
+  m_vRenderGroups.at(renderGroupIndex).push_back(std::move(textureDescriptor));
+}
+
+}// namespace GL
--- source/rendering/RenderScene.hxx
+++ source/rendering/RenderScene.hxx
@@ -1,33 +1,41 @@
 #pragma once
 
+#include <stdexcept>
 #include <string>
+
 #include <vector>
 #include <list>
 
 namespace GL {
 
-using TextureDescriptor     = unsigned int;
-using RenderGroup           = std::vector<TextureDescriptor>;
+using TextureDescriptor = unsigned int;
+using RenderGroup = std::vector<TextureDescriptor>;
 using RenderGroupCointainer = std::vector<RenderGroup>;
 
-using RenderGroupID            = unsigned int;
-using RenderGroupIDList        = std::list<RenderGroupID>;
-using RenderGroupIDContainer   = std::vector<RenderGroupID>;
+using RenderGroupID = unsigned int;
+using RenderGroupIDList = std::list<RenderGroupID>;
+using RenderGroupIDContainer = std::vector<RenderGroupID>;
 using RenderGroupNameContainer = std::unordered_map<std::string, RenderGroupID>;
 
 class RenderScene
 {
-    friend class RenderingManager
-public:
+  friend class RenderingManager;
 
-    // Get the unique ID for the freshly-baked render group
-    RenderGroupID getDistinctRenderGroupID(void) const noexcept;
+public:
+  // Get the unique ID for the freshly-baked render group
+  RenderGroupID getDistinctRenderGroupID(void) const noexcept;
+
+  // Alias render group to some unique string
+  void aliasRenderGroup(const RenderGroupID &renderGroupID, const std::string &renderGroupName) const noexcept;
+
+  // Push the texture descriptor to the render group
+  void pushToRenderGroup(const RenderGroupID &renderGroupID, const TextureDescriptor &textureDescriptor) const noexcept;
 
 private:
-    RenderGroupContainer     m_vRenderGroups;
-    RenderGroupIDList        m_lEnabledRenderGroups;
-    RenderGroupIDContainer   m_vRenderGroupIDtoInternalID;
-    RenderGroupNameContainer m_mRenderGroupNames;
+  RenderGroupContainer m_vRenderGroups;
+  RenderGroupIDList m_lEnabledRenderGroups;
+  RenderGroupIDContainer m_vRenderGroupIDtoInternalID;
+  RenderGroupNameContainer m_mRenderGroupNames;
 };
 
-} // namespace GL
+}// namespace GL
