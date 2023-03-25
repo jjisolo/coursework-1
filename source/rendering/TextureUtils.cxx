921ab7379f201db74be6996fd8c8394c1cc60c1e
jjisolo <jjisolo24@gmail.com>
2023-03-25 19:34:29 +0300

    fix(rendering): clang-formatted

--- source/rendering/TextureUtils.cxx
+++ source/rendering/TextureUtils.cxx
@@ -5,37 +5,37 @@
 
 unsigned int GL::Utils::loadTexture2D(const std::string &texturePath)
 {
-    unsigned int textureDescriptor;
+  unsigned int textureDescriptor;
 
-    glGenTextures(1, &textureDescriptor);
-    glBindTexture(GL_TEXTURE_2D, textureDescriptor);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+  glGenTextures(1, &textureDescriptor);
+  glBindTexture(GL_TEXTURE_2D, textureDescriptor);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
-    int textureWidth, textureHeight, nrChannels;
+  int textureWidth, textureHeight, nrChannels;
 
-    unsigned char *textureData = stbi_load(
-      texturePath.c_str(),
-      &textureWidth,
-      &textureHeight,
-      &nrChannels,
-      0);
+  unsigned char *textureData = stbi_load(
+    texturePath.c_str(),
+    &textureWidth,
+    &textureHeight,
+    &nrChannels,
+    0);
 
-    if (textureData) {
-      glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, textureWidth, textureHeight, 0, GL_RGB, GL_UNSIGNED_BYTE, textureData);
-      glGenerateMipmap(GL_TEXTURE_2D);
-    } else {
-      // TODO: logging
-    }
+  if (textureData) {
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, textureWidth, textureHeight, 0, GL_RGB, GL_UNSIGNED_BYTE, textureData);
+    glGenerateMipmap(GL_TEXTURE_2D);
+  } else {
+    // TODO: logging
+  }
 
-    stbi_image_free(textureData);
-    return textureDescriptor;
+  stbi_image_free(textureData);
+  return textureDescriptor;
 }
 
 unsigned int GL::Utils::releaseTexture2D(unsigned int *textureDescriptor)
 {
-    glDeleteTextures(1, textureDescriptor);
-    glBindTexture(GL_TEXTURE_2D, 0);
+  glDeleteTextures(1, textureDescriptor);
+  glBindTexture(GL_TEXTURE_2D, 0);
 }
