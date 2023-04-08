55237d53cce9ee417960d7f4a164d4478342923c
jjisolo <jjisolo24@gmail.com>
2023-04-08 22:44:07 +0300

    "feat(general):Add logging support"

--- source/main.cc
+++ source/main.cc
@@ -2,7 +2,11 @@
 #include "game.hh"
 #include "player.hh"
 
+#include "spdlog/spdlog.h"
+
 int main(int argc, char* argv[]) {
+  spdlog::info("Welcome to spdlog!");
+
   Core::Engine applicationEngine;
   Game::Board  applicationBoard;
 
