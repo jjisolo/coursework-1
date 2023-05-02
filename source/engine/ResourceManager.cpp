616fb825bf4ab948fa40130ea4753640258d2339
jjisolo <jjisolo24@gmail.com>
2023-05-02 23:11:42 +0300

    Create texture and shader loaders

--- source/engine/ResourceManager.cpp
+++ source/engine/ResourceManager.cpp
@@ -26,30 +26,103 @@ namespace Engine::Core
 		return(m_Shaders[name]);
 	}
 
-	GFX::Core::ShaderWrapper ResourceManager::getShader(const std::string& name)
+	GFX::Core::ShaderWrapper ResourceManager::getShader(const std::string& name) noexcept
 	{
 		return(m_Shaders[name]);
 	}
 
-	GFX::Core::TextureWrapper ResourceManager::loadTexture(const char* textureFileName, GLboolean alphaChannel, const std::string& name)
+	GFX::Core::TextureWrapper ResourceManager::loadTexture(const char* textureFileName, GLboolean alphaChannel, const std::string& name) noexcept
 	{
 		m_Textures[name] = loadTextureFromFile(textureFileName, alphaChannel);
 		return(m_Textures[name]);
 	}
 	
-	GFX::Core::TextureWrapper ResourceManager::getTexture(const std::string& name)
+	GFX::Core::TextureWrapper ResourceManager::getTexture(const std::string& name) noexcept
 	{
 		return(m_Textures[name]);
 	}
 
-	void  ResourceManager::release()
+	void ResourceManager::release() noexcept
 	{
-		for (auto& shader : m_Shaders)
+		for (auto& shader : m_Shaders) {
 			const GLuint shaderProgramID = shader.second.getShaderID();
 			glDeleteProgram(shaderProgramID);
+		}
+
 		for (auto& texture : m_Textures) {
 			const GLuint textureID = texture.second.getTextureID();
 			glDeleteTextures(1, &textureID);
 		}
+	}
+
+	GFX::Core::ShaderWrapper ResourceManager::loadShaderFromFile(const char* vertShaderFilename, const char* fragShaderFilename, const char* geomShaderFilename) noexcept
+	{
+		std::string vertexSourceCode;
+		std::string fragmentSourceCode;
+		std::string geometrySourceCode;
+
+		try {
+			std::ifstream vertexShaderFile(vertShaderFilename);
+			std::ifstream fragmentShaderFile(fragShaderFilename);
+
+			std::stringstream vertexShaderStream;
+			std::stringstream fragmentShaderStream;
+
+			vertexShaderStream   << vertexShaderFile.rdbuf();
+			fragmentShaderStream << fragmentShaderFile.rdbuf();
+
+			vertexSourceCode = vertexShaderStream.str();
+			fragmentSourceCode = fragmentShaderStream.str();
+
+			fragmentShaderFile.close();
+			fragmentShaderFile.close();
+
+			if (geomShaderFilename != nullptr) {
+				std::ifstream     geometryShaderFile(fragShaderFilename);
+				std::stringstream geometryShaderStream;
+
+				geometryShaderStream << geometryShaderFile.rdbuf();
+				geometrySourceCode = geometryShaderStream.str();
+
+				geometryShaderFile.close();
+			}
+		}
+		catch (std::exception& exp) {
+			m_ResourceLogger->error("Unable to read shader files");
+		}
+
+		const char* vertexSource   = vertexSourceCode.c_str();
+		const char* fragmentSource = fragmentSourceCode.c_str();
+		const char* geometrySource = geometrySourceCode.c_str();
+
+		// TODO: std::expected<ShaderWrapper, Engine::error>
+		GFX::Core::ShaderWrapper shaderWrapper;
+		if (shaderWrapper.compileShader(vertexSource, fragmentSource, geometrySource) != Engine::Error::Ok) {
+			m_ResourceLogger->error("Unable to compile shader(%s, %s, %s)", vertShaderFilename, fragShaderFilename, geomShaderFilename);
+		}
+		
+		return(shaderWrapper);
+	}
+
+	GFX::Core::TextureWrapper ResourceManager::loadTextureFromFile(const char* textureFilename, bool alpaChannel) noexcept
+	{
+		// TODO: std::expected<TextureWrapper, Engine::error>
+		GFX::Core::TextureWrapper textureWrapper;
+		if (alpaChannel) {
+			textureWrapper.setTexFormat(GL_RGBA);
+			textureWrapper.setImgFormat(GL_RGBA);
+		}
+
+		GLint imageWidth, imageHeight, imageChannels;
+		GLubyte* imageData = stbi_load(textureFilename, &imageWidth, &imageHeight, &imageChannels, 0);
+		if (imageData == nullptr) {
+			m_ResourceLogger->error("Unable to load texture %s", textureFilename);
+			// return
+		}
+
+		textureWrapper.make(imageWidth, imageHeight, imageData);
+		stbi_image_free(imageData);
+
+		return(textureWrapper);
 	}
 }
--- source/engine/ResourseManager.hpp
+++ source/engine/ResourseManager.hpp
@@ -2,12 +2,17 @@
 
 #include "unordered_map"
 #include "expected"
+#include "sstream"
+#include "fstream"
 #include "memory"
 
 #include "utility/Error.hpp"
 #include "rendering/ShaderWrapper.hpp"
 #include "rendering/TextureWrapper.hpp"
 #include "Window.hpp"
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "vendor/stb_image.h"
 
 #include "spdlog/spdlog.h"
 #include "spdlog/sinks/basic_file_sink.h"
@@ -24,16 +29,16 @@ namespace Engine::Core
 
 		static GFX::Core::ShaderWrapper getShader(const std::string& shaderName) noexcept;
 
-		static GFX::Core::TextureWrapper loadTexture(const char* textureFileName, GLboolean alphaChannel, const std::string& name);
+		static GFX::Core::TextureWrapper loadTexture(const char* textureFileName, GLboolean alphaChannel, const std::string& name) noexcept;
 
-		static GFX::Core::TextureWrapper getTexture(const std::string& name);
+		static GFX::Core::TextureWrapper getTexture(const std::string& name) noexcept;
 
-		static void release();
+		static void release() noexcept;
 
 	private:
-		static GFX::Core::ShaderWrapper  loadShaderFromFile(const char* vertShaderFilename, const char* fragShaderFilename, const char* geomShaderFilename = nullptr);
+		static GFX::Core::ShaderWrapper  loadShaderFromFile(const char* vertShaderFilename, const char* fragShaderFilename, const char* geomShaderFilename = nullptr) noexcept;
 
-		static GFX::Core::TextureWrapper loadTextureFromFile(const char* textureFilename, bool alpaChannel);
+		static GFX::Core::TextureWrapper loadTextureFromFile(const char* textureFilename, bool alpaChannel) noexcept;
 
 		static void setupLogging(void) noexcept;
 		
@@ -41,6 +46,6 @@ namespace Engine::Core
 		static std::unordered_map<std::string, GFX::Core::ShaderWrapper>  m_Shaders;
 		static std::unordered_map<std::string, GFX::Core::TextureWrapper> m_Textures;
 
-		static mutable std::shared_ptr<spdlog::logger> m_ResourceLogger;
+		static std::shared_ptr<spdlog::logger> m_ResourceLogger;
 	};
 }
