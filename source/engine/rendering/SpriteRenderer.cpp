d47c86339be88eb02ab4e41863af1bdd3f898858
jjisolo <jjisolo24@gmail.com>
2023-05-03 15:15:14 +0300

    Create sprite renderer

---
+++ source/engine/rendering/SpriteRenderer.cpp
@@ -0,0 +1,64 @@
+#include "SpriteRenderer.hpp"
+
+namespace Engine::GFX
+{
+    SpriteRenderer::SpriteRenderer(Core::ShaderWrapper& shaderWrapper)
+	{
+		m_ShaderWrapper = shaderWrapper;
+		initializeRenderPipeline();
+	}
+
+	SpriteRenderer::~SpriteRenderer()
+	{
+		glDeleteVertexArrays(1, &m_QuadVertexArray);
+	}
+
+	void SpriteRenderer::initializeRenderPipeline() noexcept
+	{
+		GLuint  vertexBuffer{};
+		GLfloat verticies[] = {
+			// Position   // Texture
+			0.0f, 1.0f,   0.0f, 1.0f,
+			1.0f, 0.0f,   1.0f, 0.0f,
+			0.0f, 0.0f,   0.0f, 0.0f,
+
+			0.0f, 1.0f,   0.0f, 1.0f,
+			1.0f, 1.0f,   1.0f, 1.0f,
+			1.0f, 0.0f,   1.0f, 0.0f
+		};
+
+		glGenVertexArrays(1, &m_QuadVertexArray);
+		glGenBuffers     (1, &vertexBuffer);
+
+		glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(verticies), verticies, GL_STATIC_DRAW);
+
+		glBindVertexArray        (m_QuadVertexArray);
+		glEnableVertexAttribArray(GL_ZERO);
+		glVertexAttribPointer    (GL_ZERO, 4, GL_FLOAT, GL_FALSE, 4 * sizeof(GLfloat), (GLvoid*)GL_ZERO);
+		glBindBuffer             (GL_ARRAY_BUFFER, GL_ZERO);
+		glBindVertexArray        (GL_ZERO);
+	}
+
+	void SpriteRenderer::renderSprite(Core::TextureWrapper& textureWrapper, glm::vec2 spritePosition, glm::vec2 spriteSize, GLfloat spriteRotation, glm::vec3 spriteColor) noexcept
+	{
+		m_ShaderWrapper.useShader();
+
+		glm::mat4 modelMatrix = glm::mat4(1.0f);
+		modelMatrix = glm::translate(modelMatrix, glm::vec3(spritePosition, 1.0f));
+		modelMatrix = glm::translate(modelMatrix, glm::vec3(0.5f * spriteSize.x, 0.5f * spriteSize.y, 0.0f));
+		modelMatrix = glm::rotate(modelMatrix, glm::radians(spriteRotation), glm::vec3(0.0f, 0.0f, 1.0f));
+		modelMatrix = glm::translate(modelMatrix, glm::vec3(-0.5f * spriteSize.x, -0.5f * spriteSize.y, 0.0f));
+		modelMatrix = glm::scale(modelMatrix, glm::vec3(spriteSize, 1.0f));
+
+		m_ShaderWrapper.setMatrix4("modelMatrix", modelMatrix);
+		m_ShaderWrapper.setVector3f("spriteColor", spriteColor);
+
+		glActiveTexture(GL_TEXTURE0);
+		textureWrapper.bind();
+
+		glBindVertexArray(m_QuadVertexArray);
+		glDrawArrays     (GL_TRIANGLES, GL_ZERO, 6);
+		glBindVertexArray(GL_ZERO);
+	}
+}
---
+++ source/engine/rendering/SpriteRenderer.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "spdlog/spdlog.h"
+#include "spdlog/sinks/basic_file_sink.h"
+
+#include "glad/glad.h"
+#include "GLFW/glfw3.h"
+
+#include "ShaderWrapper.hpp"
+#include "TextureWrapper.hpp"
+
+namespace Engine::GFX
+{
+	class SpriteRenderer
+	{
+	public:
+		SpriteRenderer(Core::ShaderWrapper& shaderWrapper);
+		~SpriteRenderer();
+
+	public:
+
+		void renderSprite(Core::TextureWrapper& textureWrapper, glm::vec2 spritePosition, glm::vec2 spriteSize = glm::vec2(10.0f, 10.0f), GLfloat spriteRotation = 0.0f,
+						  glm::vec3 spriteColor = glm::vec3(1.0f)) noexcept;
+
+	private:
+		void initializeRenderPipeline() noexcept;
+
+	private:
+		Core::ShaderWrapper m_ShaderWrapper;
+		GLuint              m_QuadVertexArray;
+	};
+}
