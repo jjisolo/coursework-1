fa7d99bfca2e3cb66527abc2e750c3aecd366099
jjisolo <jjisolo24@gmail.com>
2023-05-04 23:04:46 +0300

    Rebuild using new API

--- source/Main.cpp
+++ source/Main.cpp
@@ -1,18 +1,15 @@
 #include "engine/Application.hpp"
 #include "engine/Logger.hpp"
 
+#include "game/Program.hpp"
+
 int main(int argc, char** argv[])
 {
 	UnreferencedParameter(argc);
 	UnreferencedParameter(argv);
 
-	Engine::Logger::initialize();
-
-	auto& applicationInstance = One::Application::instance();
-	if (applicationInstance.make() == Engine::Error::Ok) {
-		applicationInstance.mainLoop();
-		applicationInstance.release();
-	} 
+	Game::GameProgram gameProgram;
+	gameProgram.execute();
 
 	return(0);
 }
